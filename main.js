d6e9c2c7b810715044cadbe8893a51e8805cbf03
Mohit-Jay-Patel <113121401+Mohit-Jay-Patel@users.noreply.github.com>
2023-02-02 19:29:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,5 +10,19 @@ var recognition = new SpeechRecognition();
     console.log(event);
     var content= event.results[0][0].transcript;
     console.log(content);
-    document.getElementById("textbox").innerHTML=content;
+    setTimeout(function()
+    {
+      document.getElementById("textbox").innerHTML=content;
+      speak();
+    },2000
+    );
+    
+    speak();
+ }
+ function speak(){
+   var synth= window.speechSynthesis;
+   var speakdata= document.getElementById("textbox").value;
+   var utterthis= new SpeechSynthesisUtterance(speakdata);
+
+   synth.speak(utterthis);
  }
