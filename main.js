d9a5824c99e4af6d484a7f5b96a0fe3ba479caf6
Mohit-Jay-Patel <113121401+Mohit-Jay-Patel@users.noreply.github.com>
2023-02-13 19:42:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,19 +10,43 @@ var recognition = new SpeechRecognition();
     console.log(event);
     var content= event.results[0][0].transcript;
     console.log(content);
-    setTimeout(function()
-    {
-      document.getElementById("textbox").innerHTML=content;
-      speak();
-    },2000
-    );
-    
+    document.getElementById("textbox").innerHTML=content;
+    if(content=="take my selfie"){
     speak();
+    }
  }
  function speak(){
    var synth= window.speechSynthesis;
-   var speakdata= document.getElementById("textbox").value;
+   var speakdata= "taking your selfie in 5 seconds";
    var utterthis= new SpeechSynthesisUtterance(speakdata);
 
    synth.speak(utterthis);
+   
+   Webcam.attach(camera);
+setTimeout(function(){
+   take_snapshot();
+   save();
+},5000);
+
+   
  }
+ Webcam.set({
+   width: 360,
+   height: 250,
+   image_format: 'png',
+   png_quality: 90
+});
+var camera= document.getElementById("webcam");
+
+function take_snapshot(){
+ Webcam.snap(function(data_uri){
+   document.getElementById("output").innerHTML="<img id='image' src='"+data_uri+"'>";
+ });
+}
+function save(){
+   var link=document.getElementById("hyperlink");
+   var image=document.getElementById("image").src;
+   link.href=image;
+   link.click();
+
+}
